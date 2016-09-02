961daa1138819c8bf02f74263a8de0da290acfca
Joseph <josephpkc@gmail.com>
2016-09-01 19:35:09 -0700

    EFfect
    updated effect class

--- DnDRPG/src/effect.cpp
+++ DnDRPG/src/effect.cpp
@@ -9,9 +9,10 @@ Effect::Effect(oid id) : CosmicObject(id,"") {
 
 }
 
-bool Effect::exec(Combat* C, oid user, std::vector<oid> targets) {
+bool Effect::exec(GameManager* G, Combat* C, oid user, std::vector<oid> targets) {
 	/* Only checks parameters for general validity */
 	if (C == nullptr) return false;
+	if (G == nullptr) return false;
 	if (user < 1) return false;
 	for (std::size_t i = 0; i < targets.size(); ++i) {
 		if (targets[i] < 1) return false;
--- DnDRPG/src/effect.h
+++ DnDRPG/src/effect.h
@@ -1,13 +1,14 @@
 #ifndef EFFECT_H
 #define EFFECT_H
 #include "combat.h"
+#include "gamemanager.h"
 #include "cosmicobject.h"
 #include <vector>
 class Effect : public CosmicObject {
 public:
 	Effect();
 	Effect(oid id);
-	virtual bool exec(Combat* C, oid user, std::vector<oid> targets);
+	virtual bool exec(GameManager* G, Combat* C, oid user, std::vector<oid> targets);
 	virtual std::string toString();
 
 private:
